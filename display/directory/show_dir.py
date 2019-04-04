44e9dc35c67f8ec580facf2819712a96578cd16d
touch <1299780716@qq.com>
2019-04-04 10:29:19 +0800

    zy

--- display/directory/show_dir.py
+++ display/directory/show_dir.py
@@ -3,10 +3,12 @@ import os, json
 import time
 import datetime
 
+
 def TimeStampToTime(timestamp):
   timestamp += 8*3600
   timeStruct = time.localtime(timestamp)
   return time.strftime('%Y-%m-%d %H:%M:%S',timeStruct)
+
 
 def get_FileSize(filePath):
   fsize = os.path.getsize(filePath)
@@ -16,19 +18,22 @@ def get_FileSize(filePath):
   else:
     return str(round(fsize/float(1024), 1)) + 'M'
 
+
 def get_FileModifyTime(filePath):
   filePath.encode("utf8")
   mtime = os.stat(filePath)
   return TimeStampToTime(mtime.st_mtime)
 
+
 def get_dirname(dirname):
   date = dirname.split('_')[-1]
   return date[:-2] + '/' + date[-2:]
 
+
 def list_dir(path, res):
   for i in os.listdir(path):
     temp_dir = os.path.join(path, i)
-    if 'static' in temp_dir:
+    if '/.' in temp_dir:
       continue
     if os.path.isdir(temp_dir):
       temp = {"dirname": temp_dir.split('/')[-1], "dirname_s": get_dirname(temp_dir.split('/')[-1]), 'child_dirs': [], 'files': []}
@@ -47,9 +52,11 @@ def list_dir(path, res):
         res['files'].append(i)
   return res
 
+
 def get_origin_dirs(dirname):
   res = {'dirname': dirname, 'child_dirs': [], 'files': []}
   return list_dir('../daily_report/' + dirname + '/', res)
 
+
 if __name__ == '__main__':
-  print(json.dumps(get_origin_dirs('pdf')))
+    print(json.dumps(get_origin_dirs('pdf')))
